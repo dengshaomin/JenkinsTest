b193e2d605f08013719e4ac1ff9c22e5bcda20be
dsm <shaomin.deng@nio.com>
2022-12-08 22:52:48 +0800

    dsm分支

--- app/src/main/java/com/code/jenkinstest/MainActivity.kt
+++ app/src/main/java/com/code/jenkinstest/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        //dsm 分支
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
